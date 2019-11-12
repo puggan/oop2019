33e15ce83867cbeb50792bf48cbd2b9fda3274c1
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-12 22:40:26 +0100

    Week 1 Exercise 10

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: w1e01 w1e02 w1e03 w1e04 w1e05 w1e06 w1e07 w1e08 w1e09
+all: w1e01 w1e02 w1e03 w1e04 w1e05 w1e06 w1e07 w1e08 w1e09 w1e10
 
 w1e01: build/w1e01.exe
 build/w1e01.exe: w1e01/w1e01.cs
@@ -35,3 +35,7 @@ build/w1e08.exe: w1e08/w1e08.cs
 w1e09: build/w1e09.exe
 build/w1e09.exe: w1e09/w1e09.cs
 	mcs w1e09/w1e09.cs -out:build/w1e09.exe
+
+w1e10: build/w1e10.exe
+build/w1e10.exe: w1e10/w1e10.cs w1e10/invader.cs
+	mcs w1e10/w1e10.cs w1e10/invader.cs -out:build/w1e10.exe
---
+++ w1e10/w1e10.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading;
+
+namespace w1e10
+{
+    internal class Program
+    {
+        string[] invader;
+        private int width;
+        private int height;
+        private int posx;
+        private int posy;
+        private int maxx;
+        private int maxy;
+
+        public static void Main(string[] args)
+        {
+            Program me = new Program();
+            while (me.draw())
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        public Program()
+        {
+            invader = invaderRows();
+            height = invader.Length;
+            width = invader[0].Length;
+            posx = 0;
+            posy = 0;
+            maxx = Console.LargestWindowWidth - width;
+            maxy = Console.LargestWindowHeight - height;
+            /*
+            Console.WriteLine($"Size: {maxx} x {maxy}");
+            Thread.Sleep(10000);
+            */
+        }
+
+        public bool draw()
+        {
+            if (posy % 2 > 0)
+            {
+                if (posx <= 0)
+                {
+                    posy++;
+                }
+                else
+                {
+                    posx--;
+                }
+            }
+            else
+            {
+                if (posx >= maxx)
+                {
+                    posy++;
+                }
+                else
+                {
+                    posx++;
+                }
+            }
+
+            Console.Clear();
+            print(posx, posy);
+            return (posy <= maxy);
+        }
+
+        public void print(int startx, int starty)
+        {
+            int y = starty;
+            foreach (string row in invader)
+            {
+                Console.SetCursorPosition(startx, y++);
+                Console.WriteLine(row);
+            }
+        }
+
+        public static string[] invaderRows()
+        {
+            int[,] invaderInts = henrikql.Invader.invader();
+            int height = invaderInts.GetLength(0);
+            int width = invaderInts.GetLength(1);
+            string[] result = new String[height];
+            
+            for(var y = 0; y < height; y++)
+            {
+                char[] charRow = new char[width];
+                for (var x = 0; x < width; x++)
+                {
+                    charRow[x] = invaderInts[y,x] > 0 ? '*' : ' ';
+                }
+
+                result[y] = new String(charRow);
+            }
+
+            return result;
+        }
+    }
+}

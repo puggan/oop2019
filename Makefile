661ae7e80d5f55b20127647c3f48c2439037ae6a
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-13 00:14:17 +0100

    Week 1 Exercise 12

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: w1e01 w1e02 w1e03 w1e04 w1e05 w1e06 w1e07 w1e08 w1e09 w1e10 w1e11
+all: w1e01 w1e02 w1e03 w1e04 w1e05 w1e06 w1e07 w1e08 w1e09 w1e10 w1e11 w1e12
 
 w1e01: build/w1e01.exe
 build/w1e01.exe: w1e01/w1e01.cs
@@ -43,3 +43,7 @@ build/w1e10.exe: w1e10/w1e10.cs w1e10/invader.cs
 w1e11: build/w1e11.exe
 build/w1e11.exe: w1e11/w1e11.cs
 	mcs w1e11/w1e11.cs -out:build/w1e11.exe
+
+w1e12: build/w1e12.exe
+build/w1e12.exe: w1e12/w1e12.cs
+	mcs w1e12/w1e12.cs -out:build/w1e12.exe
---
+++ w1e12/w1e12.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace w1e12
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<byte[,]> parts = new List<byte[,]>();
+            // @see https://pradeep1210.files.wordpress.com/2010/02/tetrisshapes5b35d.jpg
+            parts.Add(new byte[,] { { 1, 1, 1, 1 } }); // I
+            parts.Add(new byte[,] { { 1, 0, 0 }, { 1, 1, 1 } }); // J
+            parts.Add(new byte[,] { { 1, 1, 1 }, { 1, 0, 0 } }); // L
+            parts.Add(new byte[,] { { 1, 1 }, { 1, 1 } }); // O
+            parts.Add(new byte[,] { { 0, 1, 1 }, { 1, 1, 0 } }); // S
+            parts.Add(new byte[,] { { 1, 1, 1 }, { 0, 1, 0 } }); // T
+            parts.Add(new byte[,] { { 1, 1, 0 }, { 0, 1, 1 } }); // Z
+            
+            foreach(byte[,] part in parts)
+            {
+                print(part);
+                byte[,] view = part;
+                for (int r = 0; r < 4; r++)
+                {
+                    view = rotateCW(view);
+                    //view = rotateCCW(view);
+                    Console.WriteLine();
+                    print(view);
+                }
+                Console.WriteLine();
+                Console.WriteLine("--");
+                Console.WriteLine();
+            }
+        }
+
+        public static byte[,] rotateCW(byte[,] part)
+        {
+            int width = part.GetLength(0);
+            int height = part.GetLength(1);
+            byte[,] partCopy = new byte[height, width];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    partCopy[y, x] = part[width - 1 - x, y];
+                }
+            }
+
+            return partCopy;
+        }
+
+        public static byte[,] rotateCCW(byte[,] part)
+        {
+            int width = part.GetLength(0);
+            int height = part.GetLength(1);
+            byte[,] partCopy = new byte[height, width];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    partCopy[y, x] = part[x, height - 1 - y];
+                }
+            }
+
+            return partCopy;
+        }
+
+        public static void print(byte[,] part)
+        {
+            for(int y = 0; y < part.GetLength(0); y++)
+            {
+                char[] row = new char[part.GetLength(1)];
+                for(int x = 0; x < part.GetLength(1); x++)
+                {
+                    row[x] = part[y, x] > 0 ? 'X' : '.';
+                }
+
+                Console.WriteLine(row);
+            }
+        }
+    }
+}

cf7e277b53e46c8002abb3de5b4eee34ef549e03
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-12 18:33:52 +0100

    Week 1 Exercise 3

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: w1e1 w1e2
+all: w1e1 w1e2 w1e3
 
 w1e1: build/w1e1.exe
 build/w1e1.exe: w1e1/w1e1.cs
@@ -7,3 +7,7 @@ build/w1e1.exe: w1e1/w1e1.cs
 w1e2: build/w1e2.exe
 build/w1e2.exe: w1e2/w1e2.cs
 	mcs w1e2/w1e2.cs -out:build/w1e2.exe
+
+w1e3: build/w1e3.exe
+build/w1e3.exe: w1e3/w1e3.cs
+	mcs w1e3/w1e3.cs -out:build/w1e3.exe
---
+++ w1e3/w1e3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace w1e1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                if (IsPalindrome(arg))
+                {
+                    Console.WriteLine($"✔ {arg}");
+                }
+                else
+                {
+                    Console.WriteLine($"❌ {arg}");
+                }
+            }
+        }
+
+        private static bool IsPalindrome(String text)
+        {
+            string noSpaceText = Regex.Replace(text.ToLower(), @"\s+", "");
+            for (int position = 0; position * 2 < noSpaceText.Length; position++)
+            {
+                if (noSpaceText[position] != noSpaceText[noSpaceText.Length - 1 - position])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

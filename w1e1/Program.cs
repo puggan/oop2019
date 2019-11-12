70c2de5ba7faa8e4b0321f2d11ef196fd04b5365
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-12 17:35:33 +0100

    Week 1 Exercise 1

---
+++ w1e1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace w1e1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber = ReadInt("Type a number");
+            int lastNumber = ReadInt("Type another number");
+
+            Console.WriteLine();
+            Console.WriteLine($"{firstNumber} + {lastNumber} = {firstNumber + lastNumber}");            
+        }
+
+        private static int ReadInt(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                Console.WriteLine(": ");
+                var a = Console.ReadLine();
+                try
+                {
+                    return Int32.Parse(a);
+                }
+                catch (FormatException)
+                {
+                    Console.Write("That's not a number, try again");
+                }
+            }
+        }
+    }
+}

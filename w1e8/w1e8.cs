9a6322983317148dedb36ca4f5e7febbf2f36735
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-12 20:17:19 +0100

    Week 1 Exercise 8

---
+++ w1e8/w1e8.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace w1e8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = ReadInt("Please input size of tree");
+            if (size < 2)
+            {
+                Console.Error.WriteLine("Sorry, the smalles I can draw is a size 2.");
+                return;
+            }
+
+            for (int row = 0; row < size; row++)
+            {
+                int offset = size - row;
+                if (offset > 0)
+                {
+                    Console.Write(new String(' ', size - row));
+                }
+                Console.WriteLine(new String('*', 2 * row + 1));
+            }
+            Console.Write(new String(' ', size - 1));
+            Console.WriteLine(new String("[ ]"));
+        }
+
+        private static int ReadInt(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                Console.WriteLine(": ");
+                try
+                {
+                    return Int32.Parse(Console.ReadLine() ?? throw new FormatException());
+                }
+                catch (FormatException)
+                {
+                    Console.Error.WriteLine("That's not a number, try again");
+                }
+            }
+        }
+    }
+}

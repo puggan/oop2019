d7e725b08c936faef1ad9c45a25227701dbb98f4
Thorbjörn Sundragon <puggan@spiro.se>
2019-11-19 16:02:09 +0100

    Week 2 Exercise 3

---
+++ w2e03/BoxManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using w2e01;
+
+namespace w2e03
+{
+    internal class BoxManager<T> where T : Box
+    {
+        private List<T> boxes;
+
+        public BoxManager(List<T> boxes)
+        {
+            this.boxes = boxes;
+        }
+        public BoxManager() : this(new List<T>()) {}
+
+        public void Add(T box)
+        {
+            boxes.Add(box);
+        }
+
+        public void DrawInCenter()
+        {
+            int vh = Console.WindowHeight;
+            int vw  = Console.WindowWidth;
+            foreach(var box in boxes.OrderBy(a => -a.width).ThenBy(b => -b.height))
+            {
+                int left = (vw - box.width) / 2;
+                if (left < 0) left = 0;
+                int top = (vh - box.height) / 2;
+                if (top < 0) top = 0;
+                box.Draw(left, top);
+            }
+        }
+    }
+}
